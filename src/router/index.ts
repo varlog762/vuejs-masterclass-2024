358181e05a13184fab0660a9ea750644f8346c59
Gregory Pereverzev <boot762@gmail.com>
2024-12-16 15:17:53 +0300

    feat: install and configure Unplugin Vue Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-  ],
+  routes,
 })
 
 export default router
