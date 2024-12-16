435241533490f943b65bdd2a0da998df06cd06e8
Gregory Pereverzev <boot762@gmail.com>
2024-12-16 19:30:22 +0300

    feat: install supabase

--- src/pages/projects/[id].vue
+++ src/pages/projects/[id].vue
@@ -1,14 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-
-const route = useRoute()
-
-const projectId = route.params?.id
 </script>
 
 <template>
-  <h1>Project {{ projectId }}</h1>
+  <h1>Project Name</h1>
 </template>
 
 <style lang="scss" scoped></style>
