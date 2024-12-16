9db9d9d8e3a984b77f15a3f27bffe07c44aa7567
Gregory Pereverzev <boot762@gmail.com>
2024-12-16 14:51:05 +0300

    feat: add SingleProjectView.vue

---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const projectId = route.params?.id
+</script>
+
+<template>
+  <h1>Project {{ projectId }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
