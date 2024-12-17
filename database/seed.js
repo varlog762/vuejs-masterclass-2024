9e965c62b764591fc89b06ae14fc7b4be5d6157e
Gregory Pereverzev <boot762@gmail.com>
2024-12-17 18:52:44 +0300

    feat: update seed.js

--- database/seed.js
+++ database/seed.js
@@ -3,17 +3,28 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
 
-const seedProjects = async () => {
+const createProject = () => {
   const name = faker.lorem.words(3)
 
-  const { error } = await supabase.from('projects').insert({
+  return {
     name,
     slug: name.toLowerCase().replace(/ /g, '-'),
     status: faker.helpers.arrayElement(['in-progress', 'completed']),
     collaborators: faker.helpers.arrayElements([1, 2, 3]),
-  })
-
-  console.log(error)
+  }
 }
 
-await seedProjects()
+const createProjectsArray = (numberOfProjects = 1) =>
+  Array.from({ length: numberOfProjects }, createProject)
+
+const seedProjects = async (arrayLength = 1) => {
+  const projectsArray = createProjectsArray(arrayLength)
+
+  const { error } = await supabase.from('projects').insert(projectsArray)
+
+  if (error) {
+    console.error(`Error: ${error}`)
+  }
+}
+
+await seedProjects(10)
