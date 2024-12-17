f9480970b2e462a62c100e55244cbb70e89b764d
Gregory Pereverzev <boot762@gmail.com>
2024-12-17 17:14:02 +0300

    fix: use correct methods in seed.js, update packages versions

--- database/seed.js
+++ database/seed.js
@@ -4,14 +4,16 @@ import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
 
 const seedProjects = async () => {
-  const name = faker.lorem.word(3)
+  const name = faker.lorem.words(3)
 
-  await supabase.from('projects').insert({
+  const { error } = await supabase.from('projects').insert({
     name,
     slug: name.toLowerCase().replace(/ /g, '-'),
     status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    collaborators: faker.helpers.arrayElement([1, 2, 3]),
+    collaborators: faker.helpers.arrayElements([1, 2, 3]),
   })
+
+  console.log(error)
 }
 
 await seedProjects()
