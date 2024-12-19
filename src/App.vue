24e3077d809d0f57ec3d50c8d0178609dc49ed8f
Gregory Pereverzev <boot762@gmail.com>
2024-12-19 19:52:37 +0300

    feat: install Shadcn components (dropdown-menu and avatar)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabase-client'
-
-console.log(supabase)
+import Input from '@/components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 </script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex items-center justify-between px-6">
+    <form class="w-full max-w-96">
+      <Input type="text" class="w-full pl-8 bg-background" placeholder="Search..." />
+    </form>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div class="w-8 h-8 rounded-full bg-white"></div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem></DropdownMenuItem>
+        <DropdownMenuItem></DropdownMenuItem>
+        <DropdownMenuItem></DropdownMenuItem>
+        <DropdownMenuItem></DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
   <main>
     <RouterView />
   </main>
