fb07927034d10b43fa447210ccff7aff99c0af45
Gregory Pereverzev <boot762@gmail.com>
2024-12-19 18:34:05 +0300

    feat: install tailwindcss, update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [VueRouter(), vue(), vueDevTools()],
   resolve: {
     alias: {
