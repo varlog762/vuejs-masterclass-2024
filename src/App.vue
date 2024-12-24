9a7b37f0c26ef8fb66223e116a74809365c91119
Gregory Pereverzev <boot762@gmail.com>
2024-12-24 20:04:11 +0300

    feat: install iconify-icon

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 </script>
 
 <template>
@@ -18,15 +19,19 @@ import {
 
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <div class="w-8 h-8 rounded-full bg-white"></div>
+        <!-- <div class="w-8 h-8 rounded-full bg-white"></div> -->
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem></DropdownMenuItem>
-        <DropdownMenuItem></DropdownMenuItem>
-        <DropdownMenuItem></DropdownMenuItem>
-        <DropdownMenuItem></DropdownMenuItem>
+        <DropdownMenuItem>Some action 1</DropdownMenuItem>
+        <DropdownMenuItem>Some action 2</DropdownMenuItem>
+        <DropdownMenuItem>Some action 3</DropdownMenuItem>
+        <DropdownMenuItem>Some action 4</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   </nav>
