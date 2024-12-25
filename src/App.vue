7b31f9ffe4b8b9a3d5d754327fe106a19e27da71
Gregory Pereverzev <boot762@gmail.com>
2024-12-25 15:09:58 +0300

    feat: add TopNavbar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts">
-import Input from '@/components/ui/input/Input.vue'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import TopNavbar from '@/components/Layout/TopNavbar.vue'
 </script>
 
 <template>
@@ -76,31 +67,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
     </nav>
   </aside>
   <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <nav class="h-16 border-b bg-muted/40 flex gap-2 justify-between px-6 items-center">
-      <form class="relative h-fit w-full max-w-96">
-        <iconify-icon
-          class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
-          icon="lucide:search"
-        ></iconify-icon>
-        <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
-      </form>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Avatar>
-            <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </nav>
+    <TopNavbar />
     <main>
       <RouterView />
     </main>
