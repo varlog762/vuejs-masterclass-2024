ba94a7d9b0b53a6835d36056d9bb2f2749e53239
Gregory Pereverzev <boot762@gmail.com>
2024-12-25 16:46:55 +0300

    feat: add AuthLayout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Home Page</h1>
-  <router-link :to="{ name: '/projects/' }">to projects</router-link>
+  <div>
+    <h1>Home Page</h1>
+    <router-link :to="{ name: '/projects/' }">to projects</router-link>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
