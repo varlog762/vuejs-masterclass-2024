420667f0304884510443b6d9dae859377b5523ed
Gregory Pereverzev <boot762@gmail.com>
2025-01-07 14:01:36 +0300

    feat: add DataTable component

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { h, ref } from 'vue'
 
 import type { Tables } from '../../../database/types/supabase'
 import { supabase } from '@/lib/supabase-client'
+import type { ColumnDef } from '@tanstack/vue-table'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 
@@ -16,6 +17,44 @@ const tasks = ref<Tables<'tasks'>[] | null>(null)
 
   tasks.value = data
 })()
+
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+]
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  },
+]
 </script>
 
 <template>
