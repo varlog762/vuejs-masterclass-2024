049a6ea84e48f661a8278addc15044ba57122b5e
Gregory Pereverzev <boot762@gmail.com>
2025-01-07 16:07:56 +0300

    feat: update 'tasks' component

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { h, ref } from 'vue'
+import type { ColumnDef } from '@tanstack/vue-table'
 
 import type { Tables } from '../../../database/types/supabase'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
 import { supabase } from '@/lib/supabase-client'
-import type { ColumnDef } from '@tanstack/vue-table'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 
@@ -18,51 +19,51 @@ const tasks = ref<Tables<'tasks'>[] | null>(null)
   tasks.value = data
 })()
 
-interface Payment {
-  id: string
-  amount: number
-  status: 'pending' | 'processing' | 'success' | 'failed'
-  email: string
-}
-
-const payments: Payment[] = [
+const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '489e1d42',
-    amount: 125,
-    status: 'processing',
-    email: 'example@gmail.com',
-  },
-]
-
-const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: 'amount',
-    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
     cell: ({ row }) => {
-      const amount = Number.parseFloat(row.getValue('amount'))
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(amount)
-
-      return h('div', { class: 'text-right font-medium' }, formatted)
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    },
+  },
+  {
+    accessorKey: 'due_date',
+    header: () => h('div', { class: 'text-left' }, 'Due Date'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
+    },
+  },
+  {
+    accessorKey: 'project_id',
+    header: () => h('div', { class: 'text-left' }, 'Project'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+    },
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators')),
+      )
     },
   },
 ]
 </script>
 
 <template>
-  <h1>Tasks Page</h1>
-  <router-link to="/">to home</router-link>
-  <ul>
-    <li v-for="task in tasks" :key="task.id">{{ task.name }}</li>
-  </ul>
+  <DataTable v-if="tasks" :columns="columns" :data="tasks" />
 </template>
 
 <style lang="scss" scoped></style>
