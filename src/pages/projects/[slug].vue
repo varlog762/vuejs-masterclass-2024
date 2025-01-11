92217a60979072ccc9541cb60187bb2bc9dde0ff
Gregory Pereverzev <boot762@gmail.com>
2025-01-11 19:56:51 +0300

    feat: install unplugin-auto-import

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -1,7 +1,5 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts">
-import { useRoute } from 'vue-router'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>Project Name</h1>
