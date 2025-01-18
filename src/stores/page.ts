cd9ae7e2a3376f8345033c92a047b37a1261bd73
Gregory Pereverzev <boot762@gmail.com>
2025-01-18 15:59:23 +0300

    feat: implement page-store

--- src/stores/page.ts
+++ src/stores/page.ts
@@ -1,5 +1,13 @@
-import { defineStore } from 'pinia'
+import { acceptHMRUpdate, defineStore } from 'pinia'
 
-export const usePageStore = defineStore('page', () => {
-  return {}
+export const usePageStore = defineStore('page-store', () => {
+  const pageData = ref({
+    title: '',
+  })
+
+  return { pageData }
 })
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(usePageStore, import.meta.hot))
+}
