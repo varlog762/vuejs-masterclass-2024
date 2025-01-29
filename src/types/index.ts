92dbb9ef48db76eb4d6bb8c6c48db5afaa802d7e
Gregory Pereverzev <boot762@gmail.com>
2025-01-29 16:11:41 +0300

    feat: add LoginFormInputInterface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './custom-error.interface'
 export * from './extended-postgrest-error.interface'
 export * from './register-form-input.interface'
+export * from './login-form-input.interface'
---
+++ src/types/login-form-input.interface.ts
@@ -0,0 +1,4 @@
+export interface LoginFormInputInterface {
+  email: string
+  password: string
+}
