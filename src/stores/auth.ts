4b10c1e1202c22f6a49175e844fadc950f2c824c
Gregory Pereverzev <boot762@gmail.com>
2025-02-02 15:33:00 +0300

    feat: add setAuth method to user-store

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,9 +1,18 @@
-import type { User } from '@supabase/supabase-js'
+import type { Session, User } from '@supabase/supabase-js'
 import type { Tables } from 'database/types/supabase'
 
 export const useAuthStore = defineStore('auth-store', () => {
   const user = ref<User | null>(null)
   const profile = ref<Tables<'profiles'> | null>(null)
 
-  return { user, profile }
+  const setAuth = (userSession: Session | null = null) => {
+    if (!userSession) {
+      user.value = null
+      return
+    }
+
+    user.value = userSession.user
+  }
+
+  return { user, profile, setAuth }
 })
