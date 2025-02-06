34b2a5892803dc5a720b51ed4e1553d912c0b6dd
Gregory Pereverzev <boot762@gmail.com>
2025-02-06 15:35:37 +0300

    feat: update useFormErrors

--- src/composables/formErrors.ts
+++ src/composables/formErrors.ts
@@ -1,6 +1,18 @@
 import type { LoginFormInputInterface } from '@/types'
 import type { AuthError } from '@supabase/supabase-js'
 
+/**
+ * Returns an object with the following properties:
+ *
+ * - `serverError`: a ref to a string that stores the server error message.
+ * - `handleServerError`: a function that takes an `AuthError` object and sets the
+ *   `serverError` ref to the error message.
+ * - `realtimeErrors`: a ref to an object with the following properties:
+ *   - `email`: an array of strings that stores the real-time validation errors for the email field.
+ *   - `password`: an array of strings that stores the real-time validation errors for the password field.
+ * - `handleLoginForm`: a function that takes a `LoginFormInputInterface` object and sets the
+ *   `realtimeErrors` ref to the real-time validation errors for the form fields.
+ */
 export const useFormErrors = () => {
   const serverError = ref<string>('')
   const realtimeErrors = ref()
@@ -17,11 +29,26 @@ export const useFormErrors = () => {
       error.message === 'Invalid login credentials' ? 'Invalid email or password' : error.message
   }
 
-  const handleLoginForm = (formData: LoginFormInputInterface) => {
+  /**
+   * Validates the login form fields and sets the corresponding errors in the
+   * realtimeErrors state variable. If either the email or password fields have
+   * errors, the corresponding array will be populated with the error messages.
+   *
+   * @param {LoginFormInputInterface} formData The login form data.
+   */
+  const handleLoginForm = async (formData: LoginFormInputInterface) => {
     realtimeErrors.value = {
       email: [],
       password: [],
     }
+
+    const { validateEmail, validatePassword } = await import('@/utils/formValidations')
+
+    const emailErrors = validateEmail(formData.email)
+    if (emailErrors.length) realtimeErrors.value.email = emailErrors
+
+    const passwordErrors = validatePassword(formData.password)
+    if (passwordErrors.length) realtimeErrors.value.password = passwordErrors
   }
 
   return { serverError, handleServerError, realtimeErrors, handleLoginForm }
